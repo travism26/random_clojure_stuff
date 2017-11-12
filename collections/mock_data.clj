7de2bb078417fbc9dd50a950e07cf5ccbff31f34
travism26 <travismartin266@gmail.com>
2017-11-12 15:59:38 -0400

    edit structure and change some names.

---
+++ collections/mock_data.clj
@@ -0,0 +1,85 @@
+(ns collections.mock-data)
+
+(def data-stuff 
+  {
+   {
+    "id": 1,
+    "first_name": "Verne",
+    "last_name": "Oganesian",
+    "email": "voganesian0@newyorker.com",
+    "gender": "Male",
+    "ip_address": "206.44.174.25"
+    }, 
+   {
+    "id": 2,
+    "first_name": "Hildagard",
+    "last_name": "Dunnion",
+    "email": "hdunnion1@slideshare.net",
+    "gender": "Female",
+    "ip_address": "79.89.58.25"
+    }, 
+   {
+    "id": 3,
+    "first_name": "Bald",
+    "last_name": "Knoton",
+    "email": "bknoton2@geocities.jp",
+    "gender": "Male",
+    "ip_address": "41.119.138.48"
+    }, 
+   {
+    "id": 4,
+    "first_name": "Artur",
+    "last_name": "Winsom",
+    "email": "awinsom3@nbcnews.com",
+    "gender": "Male",
+    "ip_address": "54.215.145.9"
+    }, 
+   {
+    "id": 5,
+    "first_name": "Jaquith",
+    "last_name": "Tredgold",
+    "email": "jtredgold4@wikispaces.com",
+    "gender": "Female",
+    "ip_address": "241.61.120.220"
+    }, 
+   {
+    "id": 6,
+    "first_name": "Ortensia",
+    "last_name": "Asquith",
+    "email": "oasquith5@google.cn",
+    "gender": "Female",
+    "ip_address": "52.17.48.13"
+    }, 
+   {
+    "id": 7,
+    "first_name": "Rhea",
+    "last_name": "Cashmore",
+    "email": "rcashmore6@a8.net",
+    "gender": "Female",
+    "ip_address": "247.172.162.162"
+    }, 
+   {
+    "id": 8,
+    "first_name": "Tine",
+    "last_name": "Sargison",
+    "email": "tsargison7@netlog.com",
+    "gender": "Female",
+    "ip_address": "77.228.51.227"
+    }, 
+   {
+    "id": 9,
+    "first_name": "Bidget",
+    "last_name": "Lasselle",
+    "email": "blasselle8@surveymonkey.com",
+    "gender": "Female",
+    "ip_address": "20.142.142.237"
+    }, 
+   {
+    "id": 10,
+    "first_name": "Valentia",
+    "last_name": "Agney",
+    "email": "vagney9@walmart.com",
+    "gender": "Female",
+    "ip_address": "182.230.203.170"
+    }
+   })
---
+++ collections/primitive_types.clj
@@ -0,0 +1,140 @@
+(ns collections.primative-types 
+                                        ;(:require [collections.mock-data :as md])
+  )
+
+(println "hello world")
+
+
+                                        ;C-x C-e
+(clojure.string/blank? "")
+(clojure.string/blank? "hello travis")
+(clojure.string/upper-case "whats up")
+(clojure.string/lower-case "THE IS A TEST")
+(clojure.string/capitalize "sup")
+
+
+(symbol "name")
+
+(print (str `name))
+
+(name :someKeyword)
+
+(str :somekeyword)
+
+(use '[clojure.string :only (lower-case)])
+                                        ;(use '[clojure.string :only (split)])
+
+(print (lower-case "Some String Values Here: Hi, MAN, SUP, SBTREV"))
+
+(def test-data {:name "travis" :info "is a good guy eh" 
+                :something-else {:something-here "eh" :github-url "github.com/travism26" :personal-site "dont have one"}})
+
+
+(defn print-keyword [list-in key]
+  (print (first (list-in))))
+                                        ;(print-keyword test-data ":hello")
+
+                                        ;(println test-data)
+
+(defn print-vector 
+  ([n] 
+   (println n))
+  ([n k]
+   (let [key k]
+     (get n key))))
+
+(print-vector test-data)
+
+(def my-name (print-vector test-data :name))
+
+(println my-name)
+
+(def test-data 
+  {{:id 1, },
+   {:id 2}}
+  )
+
+(println data)
+
+(def data 
+  {{
+    :id 1
+    :first_name "Thadeus"
+    :last_name "Partener"
+    :email "tpartener0@skyrock.com"
+    :gender "Male"
+    :ip_address "34.151.60.9"
+    }, 
+   {
+    :id 2
+    :first_name "Solly"
+    :last_name "Killingbeck"
+    :email "skillingbeck1@ovh.net"
+    :gender "Male"
+    :ip_address "223.219.205.197"
+    }, 
+   {
+    :id 3
+    :first_name "Etti"
+    :last_name "Ruffle"
+    :email "eruffle2@mozilla.com"
+    :gender "Female"
+    :ip_address "91.92.0.5"
+    }, 
+   {
+    :id 4
+    :first_name "Vale"
+    :last_name "Culmer"
+    :email "vculmer3@bloomberg.com"
+    :gender "Female"
+    :ip_address "56.96.150.226"
+    }, 
+   {
+    :id 5
+    :first_name "Pen"
+    :last_name "Reymers"
+    :email "preymers4@cargocollective.com"
+    :gender "Female"
+    :ip_address "77.49.201.208"
+    }, 
+   {
+    :id 6
+    :first_name "Yorke"
+    :last_name "Greason"
+    :email "ygreason5@xrea.com"
+    :gender "Male"
+    :ip_address "201.135.64.163"
+    }, 
+   {
+    :id 7
+    :first_name "Vickie"
+    :last_name "Gullberg"
+    :email "vgullberg6@loc.gov"
+    :gender "Female"
+    :ip_address "41.227.15.42"
+    }, 
+   {
+    :id 8
+    :first_name "Bird"
+    :last_name "Chezier"
+    :email "bchezier7@woothemes.com"
+    :gender "Female"
+    :ip_address "80.16.145.14"
+    }, 
+   {
+    :id 9
+    :first_name "Aleece"
+    :last_name "Flucker"
+    :email "aflucker8@google.cn"
+    :gender "Female"
+    :ip_address "46.241.196.180"
+    }, 
+   {
+    :id 10
+    :first_name "Costanza"
+    :last_name "Alsford"
+    :email "calsford9@pinterest.com"
+    :gender "Female"
+    :ip_address "203.63.200.33"
+    }
+   })
