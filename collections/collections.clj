ea6f4e67c5c1b1799e4bf55c229a3ce86fa13fc8
Travis Martin <travismartin@Traviss-MacBook-Pro.local>
2017-12-20 20:16:29 -0400

    ints and some primitive type.

---
+++ collections/collections.clj
@@ -0,0 +1,7 @@
+(def v [4 3 :travis])
+
+(def v (vec [1 2 3 3]))
+
+(println v)
+
+(println (vec {:a 2 :b 23 :c "travis"}))
---
+++ collections/integers.clj
@@ -0,0 +1,12 @@
+
+(type 1.2)
+
+(Integer/parseInt "4")
+
+(Float/parseFloat "4.5")
+
+(Double/parseDouble "41.121232131")
+
+(Math/round 2.8)
+
+(int (* (rand) 35)) 
